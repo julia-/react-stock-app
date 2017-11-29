b1e77d4a469c90a742907350cca369e41066d522
julia- <julia.ryanx@gmail.com>
2017-11-30 10:26:24 +1100

    create and render StockInfo component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StockInfo from './components/StockInfo'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Wolf of React</h1>
+        <StockInfo
+          symbol='AAPL'
+          companyName='Apple Inc.'
+          primaryExchange='Nasdaq Global Select'
+          latestPrice={169.48}
+          latestSource='Close'
+          week52High={176.24}
+          week52Low={108.25}
+        />
       </div>
     );
   }
