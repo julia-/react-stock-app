abbbd304b44a78f8f1c3022c540d5c05d3d549a4
julia- <julia.ryanx@gmail.com>
2017-11-30 10:37:30 +1100

    moved data to state

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import './App.css';
 import StockInfo from './components/StockInfo'
 
 class App extends Component {
+  state = {
+    quote: {
+      symbol: 'AAPL',
+      companyName: 'Apple Inc.',
+      primaryExchange: 'Nasdaq Global Select',
+      latestPrice: 169.48,
+      latestSource: 'Close',
+      week52High: 176.24,
+      week52Low: 108.25
+    }
+  }
   render() {
+    const { quote } = this.state // same as const quote = this.state.quote
+
     return (
       <div className="App">
         <h1 className="App-title">Wolf of React</h1>
         <StockInfo
-          symbol='AAPL'
-          companyName='Apple Inc.'
-          primaryExchange='Nasdaq Global Select'
-          latestPrice={169.48}
-          latestSource='Close'
-          week52High={176.24}
-          week52Low={108.25}
+          { ...quote }
+          // symbol={quote.symbol}
+          // companyName={quote.companyName}
+          // primaryExchange={quote.primaryExchange}
+          // latestPrice={quote.latestPrice}
+          // latestSource={quote.latestSource}
+          // week52High={quote.week52High}
+          // week52Low={quote.week52Low}
         />
       </div>
     );
