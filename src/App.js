a4a0d6dcac1ef6ca5d7290772497cf53429843f4
julia- <julia.ryanx@gmail.com>
2017-11-30 11:07:59 +1100

    render quote from iex

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import StockInfo from './components/StockInfo'
+import { loadQuoteForStock } from './api/iex'
+
+loadQuoteForStock('nflx')
+  .then((quote) => {
+    console.log('Netflix: ', quote);
+  })
 
 class App extends Component {
   state = {
-    quote: {
-      symbol: 'AAPL',
-      companyName: 'Apple Inc.',
-      primaryExchange: 'Nasdaq Global Select',
-      latestPrice: 169.48,
-      latestSource: 'Close',
-      week52High: 176.24,
-      week52Low: 108.25
-    }
+    quote: null
   }
+
+  // The first time our component is rendered
+  // this method is called
+  componentDidMount() {
+    loadQuoteForStock('nflx')
+    .then((quote) => {
+      this.setState({ quote: quote })
+    })
+
+  }
+
   render() {
     const { quote } = this.state // same as const quote = this.state.quote
 
     return (
       <div className="App">
         <h1 className="App-title">Wolf of React</h1>
-        <StockInfo
-          { ...quote }
-          // symbol={quote.symbol}
-          // companyName={quote.companyName}
-          // primaryExchange={quote.primaryExchange}
-          // latestPrice={quote.latestPrice}
-          // latestSource={quote.latestSource}
-          // week52High={quote.week52High}
-          // week52Low={quote.week52Low}
-        />
+        {
+          !!quote ? (
+            <StockInfo
+              { ...quote }
+              // symbol={quote.symbol}
+              // companyName={quote.companyName}
+              // primaryExchange={quote.primaryExchange}
+              // latestPrice={quote.latestPrice}
+              // latestSource={quote.latestSource}
+              // week52High={quote.week52High}
+              // week52Low={quote.week52Low}
+            />
+          ) : (
+            <p>Loading...</p>
+          )
+        }
       </div>
     );
   }
